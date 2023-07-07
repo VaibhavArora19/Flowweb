a91de82cd03e01c93594549081cdf4b03172e2d8
dinesh11515 <dineshaitham2@gmail.com>
2023-07-07 09:53:38 +0530

    small changes to ai modal

--- frontend/context/StateContext.jsx
+++ frontend/context/StateContext.jsx
@@ -3,18 +3,19 @@ import * as fcl from "@onflow/fcl";
 
 export const AppContext = createContext();
 
-export const AppWrapper = ({children}) => {
-    const [user, setUser] = useState(null);
+export const AppWrapper = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [aiCode, setAiCode] = useState(null);
 
-    useEffect(() => fcl.currentUser.subscribe(setUser), []);
+  useEffect(() => fcl.currentUser.subscribe(setUser), []);
 
-    const sharedState = {
-        user
-    };
+  const sharedState = {
+    user,
+    aiCode,
+    setAiCode,
+  };
 
-    return (
-        <AppContext.Provider value={sharedState}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider value={sharedState}>{children}</AppContext.Provider>
+  );
+};
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Modal } from "../components/AI/Modal";
-const inter = Inter({ subsets: ["latin"] });
 import { useState } from "react";
 
 export default function Home() {
