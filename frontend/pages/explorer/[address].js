560a2c614324a3e9fae5933746f62d0a52c73fdb
VaibhavArora19 <aroravaibhav817@gmail.com>
2023-07-11 14:21:58 +0530

    Added contracts fetch in dynamic explorer page

--- frontend/pages/explorer/[address].js
+++ frontend/pages/explorer/[address].js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useContext } from 'react';
+import { AppContext } from '@/context/StateContext';
+import { getContracts } from '@/deploy/contracts';
 
 const AllContracts = () => {
+  const ctx = useContext(AppContext);
+  const [contracts, setContracts] = useState([]);
+
+  useEffect(() => {
+    if (ctx.user) {
+      async function getContractData() {
+        const contractsData = await getContracts(ctx.user);
+        const contractArray = [];
+        for (let contractKey in contractsData) {
+          contractArray.push({
+            name: contractKey,
+            contract: contractsData[contractKey],
+          });
+        }
+        setContracts(contractArray);
+      }
+
+      getContractData();
+    }
+  }, [ctx.user]);
+
   return <div>AllContracts</div>;
 };
 
