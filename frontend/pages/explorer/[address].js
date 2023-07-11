1d9e81cfa94757d35dcdca2cc35fe63414462a59
Aman-Mandal <002amankumar@gmail.com>
2023-07-11 18:32:53 +0530

    Added explorer page details

--- frontend/pages/explorer/[address].js
+++ frontend/pages/explorer/[address].js
@@ -4,6 +4,9 @@ import { useContext } from 'react';
 import { AppContext } from '@/context/StateContext';
 import Sidebar from '@/components/UI/Sidebar';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import Contract from '@/components/Explorer/Contract';
 
 const AllContracts = () => {
   const [contracts, setContracts] = useState([]);
@@ -39,7 +42,31 @@ const AllContracts = () => {
   }, [address, user]);
 
   return (
-    <div>{contracts.length > 0 && <Sidebar contractArray={contracts} />}</div>
+    // <div>{contracts.length > 0 && <Sidebar contractArray={contracts} />}</div>
+
+    <div className="w-[90%] mx-auto mt-20 font-Poppins">
+      <div className="flex gap-4 items-center mb-16">
+        <Image
+          src={'/pfp.svg'}
+          height={80}
+          width={80}
+          className="rounded-xl"
+          alt="profile"
+        />
+        <div>
+          <p className="text-xl font-semibold mb-2">@Vaibhav</p>
+          <p className="text-gray-400">{user?.addr?.slice(0, 12)}...</p>
+        </div>
+      </div>
+
+      <p className="bg-[#0f0f0f] w-fit py-3 px-10 rounded-md mb-4">
+        All contracts
+      </p>
+      <div className=" flex gap-12">
+        {contracts?.length &&
+          contracts.map(contract => <Contract contract={contract} />)}
+      </div>
+    </div>
   );
 };
 
